3b68cb7fb6c043bc37ab95a8900b34aa1f3cf653
Arpana <muriki.arpana@gmail.com>
2022-12-29 20:58:58 +0530

    Second Commit

---
+++ src/main/java/proj001/GitDemo/HandlingCheckBoxes.java
@@ -0,0 +1,11 @@
+package proj001.GitDemo;
+
+public class HandlingCheckBoxes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Git and Github..");
+		System.out.println("Checkboxes...");
+	}
+	
+}
